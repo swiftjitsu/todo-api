6895449b914bb16cbd393705bc15ac128c59d27b
Romeo Lorenzo <rlorenzo@drupios.com>
2016-03-29 00:46:45 -0700

    Added new /todos /todos/:id

--- server.js
+++ server.js
@@ -1,11 +1,56 @@
 var express = require('express');
-
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Leave work early tomorrow for video shoot',
+	completed: false
+}, {
+	id: 2,
+	description: 'Look for contact at San Diego Convention Center',
+	completed: false
+}, {
+	id: 3,
+	description: 'Get back to homebase.',
+	completed: false
+}];
 
 app.get('/',function(req,res){
 	res.send('TODO API Root.');
 });
+
+// GET /todos Request
+app.get('/todos', function(req,res){
+
+	res.json(todos);
+});
+
+// Get /todos/id
+app.get('/todos/:id', function(req, res){
+	var todosId = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	// var data = todos[(todosId - 1)];
+	todos.forEach(function(todo){
+		if(todosId === todo.id) {
+			matchedTodo = todo;
+		}
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404);
+	}
+
+
+	console.log("l: " + todos.length);
+
+	// res.json(data);
+
+
+});
+
 
 app.listen(PORT, function(){
 	console.log('ToDo App listening on ' + PORT);
