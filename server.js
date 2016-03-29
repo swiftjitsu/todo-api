81677edcb7cdcdea5f85c7089be69eda29338eb7
Romeo Lorenzo <rlorenzo@drupios.com>
2016-03-29 08:51:45 -0700

    Finished post todo

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Leave work early tomorrow for video shoot',
-	completed: false
-}, {
-	id: 2,
-	description: 'Look for contact at San Diego Convention Center',
-	completed: false
-}, {
-	id: 3,
-	description: 'Get back to homebase.',
-	completed: false
-}];
+var bodyParser = require('body-parser');
+
+var todos = [];
+
+// var todos = [{
+// 	id: 1,
+// 	description: 'Leave work early tomorrow for video shoot',
+// 	completed: false
+// }, {
+// 	id: 2,
+// 	description: 'Look for contact at San Diego Convention Center',
+// 	completed: false
+// }, {
+// 	id: 3,
+// 	description: 'Get back to homebase.',
+// 	completed: false
+// }];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
 	res.send('TODO API Root.');
@@ -50,6 +57,32 @@ app.get('/todos/:id', function(req, res){
 
 
 });
+
+// POST  /todos
+app.post('/todos', function(req,res){
+   var body = req.body;
+
+   // add id field
+   body.id = todoNextId++;
+
+   console.log(todoNextId);
+   // push body into array
+
+   var todosObj = { 
+   		"description": body.description,
+   		"completed": false
+   	}
+
+   	todosObj.id = todoNextId;
+
+   	todos.push(body);
+
+   console.log(body.description + ' description');
+
+   console.log(todos);
+   // res.json(body);
+});
+
 
 
 app.listen(PORT, function(){
